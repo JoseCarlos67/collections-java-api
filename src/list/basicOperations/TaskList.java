3ca0679b34ea49861fce7fdd365861f33799e8cd
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-11-03 21:36:59 -0300

    Primeira aula concluida

---
+++ src/list/basicOperations/TaskList.java
@@ -0,0 +1,35 @@
+package list.basicOperations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+  private List<Task> taskList;
+
+  public TaskList() {
+    this.taskList = new ArrayList<>();
+  }
+
+  public void addTask(String description) {
+    taskList.add(new Task(description));
+  }
+
+  public void removeTask(String description) {
+    List<Task> tasksToRemove = new ArrayList<>();
+
+    for (Task task : taskList) {
+      if (task.getDescription().equalsIgnoreCase(description))
+        tasksToRemove.add(task);
+    }
+
+    taskList.removeAll(tasksToRemove);
+  }
+
+  public int getTheTotalNumberOfTasks() {
+    return taskList.size();
+  }
+
+  public void obtainTasksDescriptions() {
+    System.out.println(taskList);
+  }
+}
